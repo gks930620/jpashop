4a506750103aa9589c63f5e434e64c0829691c73
CUJeong <akow283@gmail.com>
2022-08-23 09:38:50 +0900

    회원 리포지토리 개발 까지

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -15,15 +15,16 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    private String username;
+    private String name;
 
     @Embedded
     private Address address;
 
     @OneToMany(mappedBy = "member") //Order 클래스의 member 변수가 값이 변경이 되는 연관관계 주인
-    private List<ORDER> orders=new ArrayList<>();
+    private List<Order> orders=new ArrayList<>();
 
-
+    public Member(){
+    }
 
 
 }
