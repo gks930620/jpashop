b21da6d48a7c46b3cbf91cd0072c4cf95b57e6af
CUJeong <akow283@gmail.com>
2022-08-26 09:04:21 +0900

    JPA 강의 끝

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -27,4 +27,24 @@ public class OrderItem {
     private int orderPrice;
     private int count;
 
+
+    //생성메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){ // orderPrice가 할인 된 가격일 수도 있으니까 매개변수에 추가
+        OrderItem orderItem=new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //주문취소
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice(){
+        return getOrderPrice()*getCount();
+    }
+
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -29,6 +30,19 @@ public abstract class Item {
     private List<Category> categories=new ArrayList<>();
 
 
+
+    //비즈니스 로직
+
+    public void addStock(int quantity){
+        this.stockQuantity +=quantity;
+    }
+    public void removeStock(int quantity){
+        int restStock=this.stockQuantity-quantity;
+        if( restStock < 0)  {
+            throw new NotEnoughStockException("0보다 적어지는데?");
+        }
+        this.stockQuantity=restStock;
+    }
 
 
 }
